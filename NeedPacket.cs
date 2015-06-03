b463c7c36b7b61d58ba74bb472a2680f0cc7b8b9
jtorley-ap21 <jonathan.torley@apparel21.com>
2015-06-03 14:04:51 +1000

    Create NeedPacket.cs

---
+++ NeedPacket.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace RentalOffer.Core {
+	
+	public class Solution
+	{
+		public string Name;
+		public string Value;
+	}
+
+	public class Member
+	{
+		public string Id;
+		public string Type;
+		public string Name;
+	}
+
+    public class NeedPacket
+    {
+	    public string Id;
+		public string Need;
+        public List<Solution> Solutions = new List<Solution>();
+	    public DateTime Time = DateTime.Now;
+	    public Member Member;
+    }
+}
